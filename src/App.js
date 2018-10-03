08d7821a3a7c79f76bd49a5d6e5bd69263fda7b3
Manot Luijiu <manot.luijiu@gmail.com>
2018-10-03 23:20:43 +0700

    add LocalizeProvider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { LocalizeProvider } from 'react-localize-redux';
 import Toolbar from './components/Toolbar/Toolbar';
 import SideDrawer from './components/SideDrawer/SideDrawer';
 import Backdrop from './components/Backdrop/Backdrop';
@@ -6,36 +7,38 @@ import Home from './pages/Home';
 import Footer from './components/Footer/Footer';
 
 class App extends Component {
-  state = {
-    sideDrawerOpen: false
-  };
+	state = {
+		sideDrawerOpen: false
+	};
 
-  drawerTogggleClickHandler = () => {
-    this.setState(prevState => {
-      return { sideDrawerOpen: !prevState.sideDrawerOpen };
-    });
-  };
+	drawerTogggleClickHandler = () => {
+		this.setState(prevState => {
+			return { sideDrawerOpen: !prevState.sideDrawerOpen };
+		});
+	};
 
-  backdropClickHandler = () => {
-    this.setState({ sideDrawerOpen: false });
-  };
+	backdropClickHandler = () => {
+		this.setState({ sideDrawerOpen: false });
+	};
 
-  render() {
-    let backdrop;
+	render() {
+		let backdrop;
 
-    if (this.state.sideDrawerOpen) {
-      backdrop = <Backdrop click={this.backdropClickHandler} />;
-    }
-    return (
-      <div>
-        <Toolbar drawerClickHandler={this.drawerTogggleClickHandler} />
-        <SideDrawer show={this.state.sideDrawerOpen} />
-        {backdrop}
-        <Home />
-        <Footer />
-      </div>
-    );
-  }
+		if (this.state.sideDrawerOpen) {
+			backdrop = <Backdrop click={this.backdropClickHandler} />;
+		}
+		return (
+			<LocalizeProvider>
+				<div>
+					<Toolbar drawerClickHandler={this.drawerTogggleClickHandler} />
+					<SideDrawer show={this.state.sideDrawerOpen} />
+					{backdrop}
+					<Home />
+					<Footer />
+				</div>
+			</LocalizeProvider>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,22 +8,22 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import {
-  faCheckSquare,
-  faCoffee,
-  faTasks,
-  faCalculator,
-  faDatabase,
-  faDesktop
+	faCheckSquare,
+	faCoffee,
+	faTasks,
+	faCalculator,
+	faDatabase,
+	faDesktop
 } from '@fortawesome/free-solid-svg-icons';
 
 library.add(
-  fab,
-  faTasks,
-  faCalculator,
-  faDatabase,
-  faDesktop,
-  faCheckSquare,
-  faCoffee
+	fab,
+	faTasks,
+	faCalculator,
+	faDatabase,
+	faDesktop,
+	faCheckSquare,
+	faCoffee
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
